b2d79e2433f48787e5a24433f1e57ecec294dde1
koushik <ysaikoushik13@gmail.com>
2024-07-17 23:36:54 +0530

    initial

---
+++ src/models/product.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter Name"],
+    },
+    photos: [
+      {
+        public_id: {
+          type: String,
+          required: [true, "Please enter Public ID"],
+        },
+        url: {
+          type: String,
+          required: [true, "Please enter URL"],
+        },
+      },
+    ],
+
+    price: {
+      type: Number,
+      required: [true, "Please enter Price"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Please enter Stock"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please enter Category"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = mongoose.model("Product", schema);
